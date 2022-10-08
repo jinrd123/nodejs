6c8d9c5ccede9b742a2dba2772499c47a5577c87
靳荣达 <2368741842@qq.com>
2022-10-08 11:49:51 +0800

    session身份认证机制(客户端cookie结合服务器端session)以及服务器端session状态管理插件的使用

---
+++ Express_study/express-session状态管理/测试是否一个浏览器对应一个session对象.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const session = require('express-session');
+
+const app = express();
+
+//配置session中间件
+app.use(session({
+    secret: '761214Jl',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.get('/api/login', (req, res) => {
+    //输出session对象的user属性
+    console.log(req.session.user);
+    //修改session对象的uesr属性为jrd
+    req.session.user = 'jrd';
+    res.send('成功请求');
+})
+
+app.listen(80, () => {
+    console.log('服务器已经启动！');
+})
+
+/*
+    测试：
+        1.  使用edge浏览器请求http://localhost/api/login
+            服务器端输出undefined
+            再次请求http://localhost/api/login
+            服务器端输出jrd
+            说明：
+                session对象存储了第一次请求保存的user信息
+        2.  不关闭服务器，使用chrome浏览器请求http://localhost/api/login
+            服务器端输出undefined
+            说明：
+                服务器端的req.session对象是针对发请求的浏览器的
+                对于同一个浏览器发送的请求，req.session是共享的
+                不用的浏览器创建了多个不同的session对象，这些session对象是相互独立的
+*/
